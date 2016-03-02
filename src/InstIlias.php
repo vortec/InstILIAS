d34fac1563b240ebf6e2df60f0209a143266d1b9
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-03-02 15:39:02 +0100

    add load every config

--- src/InstIlias.php
+++ src/InstIlias.php
@@ -28,6 +28,7 @@ class InstIlias {
 
 	protected function run() {
 		$this->cloneIlias();
+		$this->installIlias();
 	}
 
 	protected function cloneIlias() {
@@ -35,8 +36,52 @@ class InstIlias {
 		$this->git_hub->checkoutBranch($this->gitHubConfig()->gitBranchName(), $this->server_config()->absolutePath());
 	}
 
+	protected function installIlias() {
+
+	}
+
 	protected function writeIliasConfig() {
 
+	}
+
+	protected function clientConfig() {
+		if($this->client_config === null) {
+			$this->client_config = $this->parser->read_config($this->install_config_file_data, "\InstILIAS\configs\ClientConfig");
+		}
+
+		return $this->client_config;
+	}
+
+	protected function dbConfig() {
+		if($this->db_config === null) {
+			$this->db_config = $this->parser->read_config($this->install_config_file_data, "\InstILIAS\configs\DbConfig");
+		}
+
+		return $this->db_config;
+	}
+
+	protected function gitHubConfig() {
+		if($this->git_hub_config === null) {
+			$this->git_hub_config = $this->parser->read_config($this->install_config_file_data, "\InstILIAS\configs\GitHubConfig");
+		}
+
+		return $this->git_hub_config;
+	}
+
+	protected function languageConfig() {
+		if($this->language_config === null) {
+			$this->language_config = $this->parser->read_config($this->install_config_file_data, "\InstILIAS\configs\LanguageConfig");
+		}
+
+		return $this->language_config;
+	}
+
+	protected function logConfig() {
+		if($this->log_config === null) {
+			$this->log_config = $this->parser->read_config($this->install_config_file_data, "\InstILIAS\configs\LogConfig");
+		}
+
+		return $this->log_config;
 	}
 
 	protected function serverConfig() {
@@ -47,11 +92,19 @@ class InstIlias {
 		return $this->server_config;
 	}
 
-	protected function gitHubConfig() {
-		if($this->git_hub_config === null) {
-			$this->git_hub_config = $this->parser->read_config($this->install_config_file_data, "\InstILIAS\configs\GitHubConfig");
+	protected function setupConfig() {
+		if($this->setup_config === null) {
+			$this->setup_config = $this->parser->read_config($this->install_config_file_data, "\InstILIAS\configs\SetupConfig");
 		}
 
-		return $this->git_hub_config;
+		return $this->setup_config;
+	}
+
+	protected function toolsConfig() {
+		if($this->tools_config === null) {
+			$this->tools_config = $this->parser->read_config($this->install_config_file_data, "\InstILIAS\configs\ToolsConfig");
+		}
+
+		return $this->tools_config;
 	}
 }
