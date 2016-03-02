f69a7e93892b547deb992628bba4609a10f20a8a
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-03-02 15:29:31 +0100

    add git hub action to InstILIAS.php

--- src/InstIlias.php
+++ src/InstIlias.php
@@ -5,13 +5,13 @@ class InstIlias {
 	protected $install_config_file_path;
 	protected $install_config_file_data;
 	protected $parser;
-	protected $ilias_configurator;
+	protected $ilias_installator;
 	protected $git_hub;
 
-	public function __construct($install_config_file_path, $parser, $ilias_configurator, $git_hub) {
+	public function __construct($install_config_file_path, $parser, $ilias_installator, $git_hub) {
 		$this->install_config_file_path = $install_config_file_path;
 		$this->parser = $parser;
-		$this->ilias_configurator = $ilias_configurator;
+		$this->ilias_installator = $ilias_installator;
 		$this->git_hub = $git_hub;
 
 		$this->readInstallConfigData();
@@ -27,6 +27,31 @@ class InstIlias {
 	}
 
 	protected function run() {
+		$this->cloneIlias();
+	}
 
+	protected function cloneIlias() {
+		$this->git_hub->cloneGitTo($this->gitHubConfig()->gitUrl(), $this->server_config()->absolutePath());
+		$this->git_hub->checkoutBranch($this->gitHubConfig()->gitBranchName(), $this->server_config()->absolutePath());
+	}
+
+	protected function writeIliasConfig() {
+
+	}
+
+	protected function serverConfig() {
+		if($this->server_config === null) {
+			$this->server_config = $this->parser->read_config($this->install_config_file_data, "\InstILIAS\configs\ServerConfig");
+		}
+
+		return $this->server_config;
+	}
+
+	protected function gitHubConfig() {
+		if($this->git_hub_config === null) {
+			$this->git_hub_config = $this->parser->read_config($this->install_config_file_data, "\InstILIAS\configs\GitHubConfig");
+		}
+
+		return $this->git_hub_config;
 	}
 }
