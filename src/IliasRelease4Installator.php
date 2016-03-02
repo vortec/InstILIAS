13163a24bbda9f68d1034e23749e7820b0ba0b4b
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-03-02 15:10:44 +0100

    remove constructor // add setter

--- src/IliasRelease4Installator.php
+++ src/IliasRelease4Installator.php
@@ -2,7 +2,7 @@
 namespace InstILIAS;
 
 /**
-* implementation of an ilias installator
+* implementation of an ilias installator for release 4.*
 *
 * @author Stefan Hecken <stefan.hecken@concepts-and-training.de>
 */
@@ -15,25 +15,6 @@ class IliasRelease4Installator implements \InstILIAS\interfaces\Installator {
 	protected $server_config;
 	protected $setup_config;
 	protected $tools_config;
-
-	public function __construct(configs\ClientConfig $client_config, configs\DbConfig $db_config, configs\LanguageConfig $language_config
-								, configs\LogConfig $log_config, configs\ServerConfig $server_config, configs\SetupConfig $setup_config
-								, configs\ToolsConfig $tools_config)
-	{
-		$this->client_config = $client_config;
-		$this->db_config = $db_config;
-		$this->log_config = $log_config;
-		$this->server_config = $server_config;
-		$this->setup_config = $setup_config;
-		$this->tools_config = $tools_config;
-	}
-
-	/**
-	* @inheritdoc
-	*/
-	public function checkoutIlias() {
-
-	}
 
 	/**
 	* @inheritdoc
@@ -61,5 +42,49 @@ class IliasRelease4Installator implements \InstILIAS\interfaces\Installator {
 	*/
 	public function installLanguages() {
 
+	}
+
+	/**
+	* @inheritdoc
+	*/
+	public function setConfigFiles(\InstILIAS\configs\ClientConfig $client_config, \InstILIAS\configs\DbConfig $db_config, \InstILIAS\configs\LanguageConfig $language_config
+								, \InstILIAS\configs\LogConfig $log_config, \InstILIAS\configs\ServerConfig $server_config, \InstILIAS\configs\SetupConfig $setup_config
+								, \InstILIAS\configs\ToolsConfig $tools_config)
+	{
+		$this->client_config = $client_config;
+		$this->db_config = $db_config;
+		$this->language_config = $language_config;
+		$this->log_config = $log_config;
+		$this->server_config = $server_config;
+		$this->setup_config = $setup_config;
+		$this->tools_config = $tools_config;
+	}
+
+	public function setClientConfig(\InstILIAS\configs\ClientConfig $client_config) {
+		$this->client_config = $client_config;
+	}
+
+	public function setDbConfig(\InstILIAS\configs\DbConfig $db_config) {
+		$this->db_config = $db_config;
+	}
+
+	public function setLanguageConfig(\InstILIAS\configs\LanguageConfig $language_config) {
+		$this->language_config = $language_config;
+	}
+
+	public function setLogConfig(\InstILIAS\configs\LogConfig $log_config) {
+		$this->log_config = $log_config;
+	}
+
+	public function setServerConfig(\InstILIAS\configs\ServerConfig $server_config) {
+		$this->server_config = $server_config;
+	}
+
+	public function setSetupConfig(\InstILIAS\configs\SetupConfig $setup_config) {
+		$this->setup_config = $setup_config;
+	}
+
+	public function setToolsConfig(\InstILIAS\configs\ToolsConfig $tools_config) {
+		$this->tools_config = $tools_config;
 	}
 }
