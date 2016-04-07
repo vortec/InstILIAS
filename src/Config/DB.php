9e12fa90f9213c906139e97ac8d994fe1e2d79ff
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-04-07 14:41:46 +0200

    add check of valid host

--- src/Config/DB.php
+++ src/Config/DB.php
@@ -20,7 +20,8 @@ class DB extends Base {
 	}
 
 	static $valid_engines = array(
-		"innodb");
+		"innodb"
+		,"myisam");
 
 	static $valid_encodings = array(
 		"utf8_general_ci");
@@ -31,11 +32,24 @@ class DB extends Base {
 	protected function checkValueContent($key, $value) {
 		switch($key) {
 			case "encoding":
+				return $this->checkContentValueInArray($value, self::$valid_encodings);
+				break;
 			case "engine":
 				return $this->checkContentValueInArray($value, self::$valid_engines);
+				break;
+			case "host":
+				return $this->checkContentHost($value);
 				break;
 			default:
 				return parent::checkValueContent($key, $value);
 		}
+	}
+
+	protected function checkContentHost($value) {
+		if(substr_count($value, " ")) {
+			return false;
+		}
+
+		return true;
 	}
 }
