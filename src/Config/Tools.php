e1c28bf632474e8f1f5905395e74f0248881259e
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-04-07 10:46:43 +0200

    change tools config and tests

--- src/Config/Tools.php
+++ src/Config/Tools.php
@@ -5,105 +5,15 @@ namespace InstILIAS\Config;
  * Configuration for the tools required by ILIAS.
  */
 class Tools extends Base {
-	/**
-	 * @var	string
+		/**
+	 * @inheritdocs
 	 */
-	protected $convert;
-
-	/**
-	 * @var	string
-	 */
-	protected $zip;
-
-	/**
-	 * @var	string
-	 */
-	protected $unzip;
-
-	/**
-	 * @var	string
-	 */
-	protected $java;
-
-	const NAME = "tools";
-
-	/**
-	* sets the convert
-	*
-	* @param string
-	*/
-	public function setConvert($convert) {
-		assert(is_string($convert));
-
-		$this->convert = $convert;
-	}
-
-	/**
-	* gets the convert
-	*
-	* @return string
-	*/
-	public function convert() {
-		return $this->convert;
-	}
-
-	/**
-	* sets the zip
-	*
-	* @param string
-	*/
-	public function setZip($zip) {
-		assert(is_string($zip));
-
-		$this->zip = $zip;
-	}
-
-	/**
-	* gets the zip
-	*
-	* @return string
-	*/
-	public function zip() {
-		return $this->zip;
-	}
-
-	/**
-	* sets the unzip
-	*
-	* @param string
-	*/
-	public function setUnzip($unzip) {
-		assert(is_string($unzip));
-
-		$this->unzip = $unzip;
-	}
-
-	/**
-	* gets the unzip
-	*
-	* @return string
-	*/
-	public function unzip() {
-		return $this->unzip;
-	}
-
-	/**
-	* sets the java
-	*
-	* @param string
-	*/
-	public function setJava($java) {
-		assert(is_string($java));
-
-		$this->java = $java;
-	}
-
-	/**
-	* gets the java
-	*
-	* @return string
-	*/
-	public function java() {
-		return $this->java;
+	public static function fields() {
+		return array
+			( "convert"	=> "string"
+			, "zip"		=> "string"
+			, "unzip"	=> "string"
+			, "java"	=> "string"
+			);
 	}
 }
---
+++ tests/ToolsConfigTest.php
@@ -0,0 +1,93 @@
+<?php
+
+use \InstILIAS\Config\Tools;
+
+class ToolConfigTest extends PHPUnit_Framework_TestCase{
+	public function test_not_enough_params() {
+		try {
+			$config = new Tools();
+			$this->assertFalse("Should have raised.");
+		}
+		catch (\InvalidArgumentException $e) {}
+	}
+
+	/**
+	 * @dataProvider	ToolsConfigValueProvider
+	 */
+	public function test_ToolsConfig($convert, $zip, $unzip, $java, $valid) {
+		if ($valid) {
+			$this->_test_valid_ToolsConfig($convert, $zip, $unzip, $java);
+		}
+		else {
+			$this->_test_invalid_ToolsConfig($convert, $zip, $unzip, $java);
+		}
+	}
+
+	public function _test_valid_ToolsConfig($convert, $zip, $unzip, $java) {
+		$config = new Tools($convert, $zip, $unzip, $java);
+		$this->assertEquals($convert, $config->convert());
+		$this->assertEquals($zip, $config->zip());
+		$this->assertEquals($unzip, $config->unzip());
+		$this->assertEquals($java, $config->java());
+	}
+
+	public function _test_invalid_ToolsConfig($convert, $zip, $unzip, $java) {
+		try {
+			$config = new Tools($convert, $zip, $unzip, $java);
+			$this->assertFalse("Should have raised.");
+		}
+		catch (\InvalidArgumentException $e) {}
+	}
+
+	public function ToolsConfigValueProvider() {
+		$ret = array();
+		foreach ($this->convertProvider() as $convert) {
+			foreach ($this->zipProvider() as $zip) {
+				foreach ($this->unzipProvider() as $unzip) {
+					foreach ($this->javaProvider() as $java) {
+						$ret[] = array
+							( $convert[0], $zip[0], $unzip[0], $java[0]
+							, $convert[1] && $zip[1] && $unzip[1] && $java[1]);
+					}
+				}
+			}
+		}
+		return $ret;
+	}
+
+	public function convertProvider() {
+		return array(
+				array("/opt/ImageMagick", true)
+				, array(5, false)
+				, array(true, false)
+				, array(array(), false)
+			);
+	}
+
+	public function zipProvider() {
+		return array(
+				array(5, false)
+				, array(true, false)
+				, array("/usr/bin/zip", true)
+				, array(array(), false)
+			);
+	}
+
+	public function unzipProvider() {
+		return array(
+				array(5, false)
+				, array("/usr/bin/unzip", true)
+				, array(true, false)
+				, array(array(), false)
+			);
+	}
+
+	public function javaProvider() {
+		return array(
+				array(5, false)
+				, array(true, false)
+				, array(array(), false)
+				, array("/usr/bin/java", true)
+			);
+	}
+}
